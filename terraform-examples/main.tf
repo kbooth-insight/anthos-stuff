b8550d37b29b2727060771dadf043e3c3925c9be
Kevin Booth <kbooth.booth@insight.com>
2020-09-16 10:05:12 -0400

    updates

--- terraform-examples/main.tf
+++ terraform-examples/main.tf
@@ -32,7 +32,7 @@ module "gke" {
       autoscaling  = false
       auto_upgrade = true
       # ASM requires minimum 4 nodes and e2-standard-4
-      node_count   = 4
+      node_count   = 2
       machine_type = "e2-standard-4"
     },
   ]
@@ -44,5 +44,17 @@ module "asm" {
   cluster_endpoint = module.gke.endpoint
   project_id       = var.project_id
   location         = module.gke.location
-  gke_hub_sa_name = "test-hub"
+  gke_hub_sa_name = "${var.prefix}-sa"
+}
+
+module "acm" {
+  source           = "terraform-google-modules/kubernetes-engine/google//modules/acm"
+  project_id       = var.project_id
+  location         = module.gke.location
+  cluster_name     = module.gke.name
+  sync_repo        = var.acm_sync_repo
+  sync_branch      = var.acm_sync_branch
+  policy_dir       = var.acm_policy_dir
+  cluster_endpoint = module.gke.endpoint
+  operator_path    = var.operator_path
 }
--- terraform-examples/variables.tf
+++ terraform-examples/variables.tf
@@ -6,3 +6,26 @@ variable vpc {}
 variable subnet {}
 variable pod_cidr_range {}
 variable services_cidr_range {}
+variable "acm_sync_repo" {
+  description = "Anthos config management Git repo"
+  type        = string
+  default     = "git@github.com:GoogleCloudPlatform/csp-config-management.git"
+}
+
+variable "acm_sync_branch" {
+  description = "Anthos config management Git branch"
+  type        = string
+  default     = "1.0.0"
+}
+
+variable "acm_policy_dir" {
+  description = "Subfolder containing configs in ACM Git repo"
+  type        = string
+  default     = "foo-corp"
+}
+
+variable "operator_path" {
+  description = "Path to the operator yaml config. If unset, will download from GCS releases."
+  type        = string
+  default     = null
+}
